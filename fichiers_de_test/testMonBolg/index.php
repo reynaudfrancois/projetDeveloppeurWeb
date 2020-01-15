07712d997239864a097f377895456bd4bc748604
reynaudfrancois <freynaut@laposte.net>
2020-01-15 06:38:05 +0100

    comments add

--- fichiers_de_test/testMonBolg/index.php
+++ fichiers_de_test/testMonBolg/index.php
@@ -1,28 +1,34 @@
 <?php
-	try {
-		$db  = new PDO("mysql:host=localhost;dbname=blog;charset=utf8", "freynaut", "admin2018");
-	}
-	catch(Exception $e) {
-		die("Erreur : ".$e->getMessage());		
-	}
 
-		// on prépare la requête de compatge des messages
-	$retour=$db->prepare("SELECT COUNT(*) AS nbArticles FROM posts");
+// ON APPELLE LA BDD
 
-	//on execute la requete
-	$retour->execute();
+try {
+	$db  = new PDO("mysql:host=localhost;dbname=blog;charset=utf8", "freynaut", "admin2018");
+}
+catch(Exception $e) {
+	die("Erreur : ".$e->getMessage());		
+}
 
-	//on renvoie le résultat sous forme de tableau
-	$donnees=$retour->fetch();
+// COMPTAGE DES MESSAGES
 
-	//on récupère le nombre total d'articles dans la varialble $totalArticles
-	$totalArticles=$donnees["nbArticles"];
+// on prépare la requête de compatge des messages
+$retour=$db->prepare("SELECT COUNT(*) AS nbArticles FROM posts");
 
-	// on choisit le nombre maximum d'articles présentés par page
-	$nbArticlesPerPage = 3;
+//on execute la requete
+$retour->execute();
 
-	//on calcule le nombre de pages nécessaires en divisant le total des articles par le nombre d'articles par page ; la fonction 'ceil' arrondit à l'entier supérieur
-	$nbPages=ceil($totalArticles/$nbArticlesPerPage);
+//on renvoie le résultat sous forme de tableau
+$donnees=$retour->fetch();
+
+//on récupère le nombre total d'articles dans la varialble $totalArticles
+$totalArticles=$donnees["nbArticles"];
+
+// on choisit le nombre maximum d'articles présentés par page
+$nbArticlesPerPage = 3;
+
+//on calcule le nombre de pages nécessaires en divisant le total des articles par le nombre d'articles par page ; la fonction 'ceil' arrondit à l'entier supérieur
+$nbPages=ceil($totalArticles/$nbArticlesPerPage);
+
 ?>
 
 
@@ -55,30 +61,33 @@
 
 		<?php
 
-			echo "<nav class='center'>PAGES : ";
-	for ($i=1 ; $i<=$nbPages ; $i++) {
-		echo "<a href='index.php?page=" . $i . "'>" . $i . "</a> ";
-	}
-	echo "</nav>";
+		// PAGINATION
 
-	// ctype_digit vérifie si tous les carctères de la chaine sont des chiffres
-	if(!empty($_GET["page"]) AND ctype_digit($_GET["page"]) AND $_GET["page"]<=$nbPages AND $_GET["page"]>=1) {
-		$page=$_GET["page"];
-	} else {
-		$_GET["page"]=1;
-		$page=$_GET["page"];
-	}
+		echo "<nav class='center'>PAGES : ";
+		for ($i=1 ; $i<=$nbPages ; $i++) {
+			echo "<a href='index.php?page=" . $i . "'>" . $i . "</a> ";
+		}
+		echo "</nav>";
 
-	echo "<h3>Page " . $page . "</h3>";
-	var_dump(is_numeric($_GET["page"]));
-	
+		// ctype_digit vérifie si tous les carctères de la chaine sont des chiffres
+		if(!empty($_GET["page"]) AND ctype_digit($_GET["page"]) AND $_GET["page"]<=$nbPages AND $_GET["page"]>=1) {
+			$page=$_GET["page"];
+		} else {
+			$_GET["page"]=1;
+			$page=$_GET["page"];
+		}
 
-	var_dump(ctype_digit($_GET["page"]));
+		echo "<h3>Page " . $page . "</h3>";
+		var_dump(is_numeric($_GET["page"]));
+		
 
-	$firstArticleDisplayed = $nbArticlesPerPage*($page-1);
+		var_dump(ctype_digit($_GET["page"]));
 
+		$firstArticleDisplayed = $nbArticlesPerPage*($page-1);
 
-		//NE PAS OUBLIER DE METTRE DES DOUBLES GUILLEMETS QUAND ON UTILISE LE SYMBOLE $ DANS UNE REQUETE
+		// AFFICHAGE DES POSTS
+
+		// (NE PAS OUBLIER DE METTRE DES DOUBLES GUILLEMETS QUAND ON UTILISE LE SYMBOLE $ DANS UNE REQUETE)
 		$reponse=$db->query("SELECT * FROM posts ORDER BY id DESC LIMIT $firstArticleDisplayed, $nbArticlesPerPage");
 
 		while ($donnees=$reponse->fetch()){
@@ -106,12 +115,37 @@
 
 		<footer>
 
-			<?php include "view/pagination.php"; ?>
+			<?php
+
+	//  PAGINATION
+			
+	echo "<nav class='center'>PAGES : ";
+	for ($i=1 ; $i<=$nbPages ; $i++) {
+		echo "<a href='index.php?page=" . $i . "'>" . $i . "</a> ";
+	}
+	echo "</nav>";
+
+	// ctype_digit vérifie si tous les carctères de la chaine sont des chiffres
+	if(!empty($_GET["page"]) AND ctype_digit($_GET["page"]) AND $_GET["page"]<=$nbPages AND $_GET["page"]>=1) {
+		$page=$_GET["page"];
+	} else {
+		$_GET["page"]=1;
+		$page=$_GET["page"];
+	}
+
+	echo "<h3>Page " . $page . "</h3>";
+	var_dump(is_numeric($_GET["page"]));
+	
+
+	var_dump(ctype_digit($_GET["page"]));
+	
+
+?>
 			
 			<div id="findMe">				
 				<h3>RETROUVEZ-MOI</h3>
 				<div>
-					<a href="contact.php"><i class="fas fa-envelope"></i></a>
+					<a href="mailto:freynaut@laposte.net"><i class="fas fa-envelope"></i></a>
 					<a href="https://www.facebook.com/fran.cisco.376043" target="new"><i class="fab fa-facebook-square"></i></a>
 					<a href="https://www.instagram.com/n0mmade" target="new"><i class="fab fa-instagram"></i></a>
 					<a href="https://www.youtube.com/channel/UCnp9vbWd4sw0SnbX8Nw0EIQ" target="new"><i class="fab fa-youtube"></i></a>
--- monBlog/footer.inc.php
+++ monBlog/footer.inc.php
@@ -5,7 +5,7 @@
 			<div id="findMe">				
 				<h3>RETROUVEZ-MOI</h3>
 				<div>
-					<a href="contact.php"><i class="fas fa-envelope"></i></a>
+					<a href="mailto:freynaut@laposte.net"><i class="fas fa-envelope"></i></a>
 					<a href="https://www.facebook.com/fran.cisco.376043" target="new"><i class="fab fa-facebook-square"></i></a>
 					<a href="https://www.instagram.com/n0mmade" target="new"><i class="fab fa-instagram"></i></a>
 					<a href="https://www.youtube.com/channel/UCnp9vbWd4sw0SnbX8Nw0EIQ" target="new"><i class="fab fa-youtube"></i></a>

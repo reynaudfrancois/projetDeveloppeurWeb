f84d1573a88467dbed38725f4fb1423d590a90fa
reynaudfrancois <freynaut@laposte.net>
2020-01-15 15:29:31 +0100

    template folder creation

---
+++ fichiers_de_test/testMonBolg/controller/controller.php
@@ -0,0 +1,41 @@
+<?php
+require "model/model.php";
+
+function postsView() {
+	// PAGE ACTUELLEMENT AFFICHEE
+	function actualPage() {
+		$nbPostsPerPage = nbPostsPerPage();
+		$nbPosts = nbPosts();
+		$nbPages = nbPages($nbPosts, $nbPostsPerPage);
+		if(!empty($_GET["page"]) AND ctype_digit($_GET["page"]) AND $_GET["page"]<=$nbPages AND $_GET["page"]>=1) {
+			$page=$_GET["page"];
+		} else {
+			$_GET["page"]=1;
+			$page=$_GET["page"];
+		}
+		return $page;
+	}
+
+	// NB DE POSTS PAR PAGE
+	function nbPostsPerPage() {
+		$nbPostsPerPage = 3;
+		return $nbPostsPerPage;
+	}
+
+	// PREMIER POST AFFICHE
+	function firstPostDisplayed($nbPostsPerPage) {
+		//$nbPostsPerPage = nbPostsPerPage();
+		$page = actualPage();
+		$firstPostDisplayed = $nbPostsPerPage*($page-1);
+		return $firstPostDisplayed;
+	}
+
+	// NB DE PAGES AFFICHANT LES POSTS
+	function nbPages($nbPosts, $nbPostsPerPage) {
+		$nbPages=ceil($nbPosts/$nbPostsPerPage);
+		return $nbPages;
+	}
+
+	require "view/postsView.php";
+	
+}
---
+++ fichiers_de_test/testMonBolg/view/template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="view/css/normalize-css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="view/fontawesome/css/all.css" />
+		<link rel="stylesheet" href="view/css/style.css" />
+		<title><?= $title ?></title>
+	</head>
+
+	<body>
+        <?= $content ?>
+    </body>
+
+</html>

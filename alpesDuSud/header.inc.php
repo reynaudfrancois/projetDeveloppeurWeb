0adc7dbf4a4dc12932b88932124fc1ff8020c45f
reynaudfrancois <freynaut@laposte.net>
2020-01-17 16:13:25 +0100

    improvement of presentation of files

--- alpesDuSud/header.inc.php
+++ alpesDuSud/header.inc.php
@@ -4,9 +4,8 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<!--<link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">-->
 		<link rel="stylesheet" type="text/css" href="view/css/normalize-css/normalize.css" />
-		<link rel="stylesheet" type="text/css" href="view/fontawesome/css/all.css" />
+		<!--<link rel="stylesheet" type="text/css" href="view/fontawesome/css/all.css" />-->
 		<link rel="stylesheet" href="view/css/style.css" />
 		<title>La traversée des Alpes du Sud</title>
 	</head>

3e070b976f03f1d1504459e7b71930e7437d1ee7
reynaudfrancois <freynaut@laposte.net>
2020-01-30 09:14:02 +0100

    correction on links on header.inc.php on alpesDuSud

--- module_a_alpesDuSud/view/header.inc.php
+++ module_a_alpesDuSud/view/header.inc.php
@@ -4,9 +4,9 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" type="text/css" href="/projetDeveloppeurWeb/portfolio/module_a_alpesDuSud/public/css/normalize-css/normalize.css" />
-		<link rel="stylesheet" type="text/css" href="/projetDeveloppeurWeb/portfolio/module_a_alpesDuSud/public/fontawesome/css/all.css" />
-		<link rel="stylesheet" href="/projetDeveloppeurWeb/portfolio/module_a_alpesDuSud/public/css/style.css" />
+		<link rel="stylesheet" type="text/css" href="/projetDeveloppeurWeb/module_a_alpesDuSud/public/css/normalize-css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="/projetDeveloppeurWeb/module_a_alpesDuSud/public/fontawesome/css/all.css" />
+		<link rel="stylesheet" href="/projetDeveloppeurWeb/module_a_alpesDuSud/public/css/style.css" />
 		<title>La traversée des Alpes du Sud</title>
 	</head>
 
@@ -15,12 +15,12 @@
 		<header>
 
 			<nav>
-				<a href="/projetDeveloppeurWeb/portfolio/module_a_alpesDuSud/index.php">Accueil</a>
-				<a href="/projetDeveloppeurWeb/portfolio/module_a_alpesDuSud/view/mountains.php">Massifs</a>
-				<a href="/projetDeveloppeurWeb/portfolio/module_a_alpesDuSud/view/path.php">Itinéraire</a>
-				<a href="/projetDeveloppeurWeb/portfolio/module_a_alpesDuSud/view/story.php">Récit</a>
-				<a href="/projetDeveloppeurWeb/portfolio/module_a_alpesDuSud/view/photos.php">Photos</a>
-				<a href="/projetDeveloppeurWeb/portfolio/module_a_alpesDuSud/view/comments.php">Commentaires</a>
+				<a href="/projetDeveloppeurWeb/module_a_alpesDuSud/index.php">Accueil</a>
+				<a href="/projetDeveloppeurWeb/module_a_alpesDuSud/view/mountains.php">Massifs</a>
+				<a href="/projetDeveloppeurWeb/module_a_alpesDuSud/view/path.php">Itinéraire</a>
+				<a href="/projetDeveloppeurWeb/module_a_alpesDuSud/view/story.php">Récit</a>
+				<a href="/projetDeveloppeurWeb/module_a_alpesDuSud/view/photos.php">Photos</a>
+				<a href="/projetDeveloppeurWeb/module_a_alpesDuSud/view/comments.php">Commentaires</a>
 			</nav>
 
 			<div id="title">
@@ -29,6 +29,6 @@
 				<h3>par François REYNAUD</h3>
 			</div>
 
-			<div id="blog"><a href="/projetDeveloppeurWeb/portfolio/module_c_monBlog" id="linkBlog" target="new">Pour suivre mon fil d'actualités sur mon blog, cliquez ici.</a></div>
+			<div id="blog"><a href="/projetDeveloppeurWeb/module_c_monBlog" id="linkBlog" target="new">Pour suivre mon fil d'actualités sur mon blog, cliquez ici.</a></div>
 
 		</header>

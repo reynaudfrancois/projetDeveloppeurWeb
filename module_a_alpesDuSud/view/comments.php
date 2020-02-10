a93124ebf0b963762605d569763893a172024cc7
reynaudfrancois <freynaut@laposte.net>
2020-02-10 20:24:09 +0100

    adding of a bloc try/catch

--- module_a_alpesDuSud/view/comments.php
+++ module_a_alpesDuSud/view/comments.php
@@ -1,31 +1,34 @@
 <?php
 
 require "../controller/commentsController.php";
-
-if (isset($_GET["action"])) {
-	if ($_GET["action"] == "addComment") {
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			if (isset($_POST["name"]) AND trim($_POST["name"])!="" AND isset($_POST["firstname"]) AND trim($_POST["firstname"])!="" AND isset($_POST["content"]) AND trim($_POST["content"])!="") {
-				$name = htmlspecialchars($_POST["name"]);
-				$firstname = htmlspecialchars($_POST["firstname"]);
-				$content = htmlspecialchars($_POST["content"]);
-				if (isset($_POST["email"]) AND trim($_POST["email"])!="") {
-					$email = htmlspecialchars($_POST["email"]);
+try {
+	if (isset($_GET["action"])) {
+		if ($_GET["action"] == "addComment") {
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				if (isset($_POST["name"]) AND trim($_POST["name"])!="" AND isset($_POST["firstname"]) AND trim($_POST["firstname"])!="" AND isset($_POST["content"]) AND trim($_POST["content"])!="") {
+					$name = htmlspecialchars($_POST["name"]);
+					$firstname = htmlspecialchars($_POST["firstname"]);
+					$content = htmlspecialchars($_POST["content"]);
+					if (isset($_POST["email"]) AND trim($_POST["email"])!="") {
+						$email = htmlspecialchars($_POST["email"]);
+					} else {
+						$email = "UNKNOWN";
+					}
+					addComment($name, $firstname, $email, $content);
+					//header("Location: comments.php?action=viewComments");
 				} else {
-					$email = "UNKNOWN";
+					$error = "<p>Vous devez obligatoirement rentrer un nom, un prénom, et un commentaire !</p>";
+					viewComments($error);
 				}
-				addComment($name, $firstname, $email, $content);
-				//header("Location: comments.php?action=viewComments");
-			} else {
-				$error = "<p>Vous devez obligatoirement rentrer un nom, un prénom, et un commentaire !</p>";
-				viewComments($error);
 			}
+		} else if ($_GET["action"] == "viewComments") {
+			$error ="";
+			viewComments($error);
 		}
-	} else if ($_GET["action"] == "viewComments") {
-		$error ="";
+	} else {
+		$error = "";
 		viewComments($error);
 	}
-} else {
-	$error = "";
-	viewComments($error);
+} catch(Exception $e) {
+	echo "Erreur : " . $e->getMessage();
 }

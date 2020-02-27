e38b2b69efa04cfde1f22b506c28a4d3274b4505
reynaudfrancois <freynaut@laposte.net>
2020-02-27 16:16:34 +0100

    add images an titles in the slider of alpesDuSud
    add images in slider of alpesDuSud + add of titles of these images in slider.js

--- module_a_alpesDuSud/public/javascript/slider.js
+++ module_a_alpesDuSud/public/javascript/slider.js
@@ -5,7 +5,7 @@ nav.style.width = "100%";
 document.getElementById("title").style.display = "none";
 document.getElementById("blog").style.display = "none";
 
-var n = 51;
+var n = 77;
 
 var images = new Array(n);
 var i, j;
@@ -21,7 +21,9 @@ tableTitlesImages.push("Sospel", "Bivouac", "Pause à Moulinet", "Panorama sur l
 tableTitlesImages.push("Vallon de Bramafam", "Rimplas", "Petite pause", "Saint-Sauveur-sur-Tinée", "Roure", "Vallée de la Tinée", "Plateau de Longon", "Vallon de Vionène", "Hameau de Vignols", "Mont Longon");
 tableTitlesImages.push("Vacherie de Roubion", "Col des Moulines", "Mont Mounier - Versant Sud", "Mont Mounier - Versant Nord", "Roya", "Vallon de Roya", "Cime de Las Donnas", "Auron", "Saint-Etienne-de-Tinée", "Aurore sur la Tinée");
 tableTitlesImages.push("Cime de la Bercha", "Saint-Dalmas-le-Selvage", "Vallon de Saint-Dalmas", "Vallon de Gialorgues", "Bousiéyas", "Ruines du Camp des Fourches", "Lac de derrière la Croix", "Descente sur névé", "Pas de la Cavale", "Sale temps pour marcher");
-tableTitlesImages.push("Larche");
+tableTitlesImages.push("Larche", "Tête de Viraysse", "Haute vallée de l'Ubayette", "Fort de Mallemort", "Saint-Ours", "A l'attaque du Col de Mirandol", "Arrivée au Col de Mirandol", "Pont du Châtelet", "Haute vallée de l'Ubaye", "Mauvais temps de  retour");
+tableTitlesImages.push("Col Tronchet", "Cascade de la Pisse", "Anes", "Ceillac", "Bramousse", "En route vers Furfande", "Combe du Queyras", "Alpage de Furfande", "Refuge de Furfande", "Balcon de Furfande");
+tableTitlesImages.push("Hauts sommets du Queyras", "Chalets de Furfande", "Lac du Lauzet", "Vallée de la Durance", "Mont-Dauphin", "Le Guil en dessous de Guillestre", "Le Guil en dessous de Mont-Dauphin");
 
 var titleImage = document.getElementById("titleImage");
 titleImage.innerHTML = tableTitlesImages[0];

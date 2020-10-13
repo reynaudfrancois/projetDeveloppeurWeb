67766809c7c7a26373f6e306685cdc477763892d
reynaudfrancois <freynaut@laposte.net>
2020-10-13 11:06:58 +0200

    Changes because access denied
    Changes because access denied "for user "freynaut" on by database of Weendo

--- module_c_monBlog/model/model.php
+++ module_c_monBlog/model/model.php
@@ -58,7 +58,7 @@ function postComment ($postId, $name, $firstname, $email, $content) {
 
 function dbConnect() {
 	try {
-		$db  = new PDO("mysql:host=localhost;dbname=blog;charset=utf8", "freynaut", "admin2018");
+		$db  = new PDO("mysql:host=localhost;dbname=francoi3_blog;charset=utf8", "francoi3", "F9WOuR;)7u9lx8");
 		return $db;
 	}
 	catch(Exception $e) {
